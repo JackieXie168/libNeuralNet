a35fc7c29ac82a50ad74287eb76832be58fc7bfd
Stuart McIntosh <mcintst@yahoo.com>
2015-04-20 21:48:14 +0800

    Initial Commit

---
+++ OpenNNTest/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  OpenNNTest
+//
+//  Created by Stuart McIntosh on 20/4/15.
+//  Copyright (c) 2015 Stuart McIntosh. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
